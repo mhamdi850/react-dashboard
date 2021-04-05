906f73f916ba6de8e4054fa1123200e4aa14f974
mhamdi850 <mhamdi.a@live.fr>
2021-04-05 21:48:04 +0200

    backend

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Countries from "./Components/Countries";
+import Filter from "./Components/Filter";
 import navBar from "./Components/Navigate/navBar";
 import data from "./data.json";
 import './index.css'
@@ -10,17 +11,62 @@ class App extends React.Component {
     super()
     this.state= {
       countries: data.countries,
-      year:"",
+      n:"",
       sort:"",
     }
   }
+  sortCountries = (e) => {
+
+    const sort=e.target.value
+    this.setState((state) => ({
+      sort:sort, 
+      countries: this.state.countries.slice().sort((a,b)=>
+        sort === "lowest"
+        ? a.amount > b.amount
+          ? 1
+          :-1
+        : sort === "highest"
+        ? a.alount < b.amount
+          ? 1
+          : -1
+        : a._id > b._id
+          ? 1
+          : -1        
+      ),
+    }))
+  }
+
+  filterCountries = (e) => {
+    if(e.target.value === ""){
+      this.setState({n:e.target.value, countries:data.countries})
+    } else{
+      this.setState({
+        n: e.target.value,
+        countries: data.countries.filter(
+          (country) => country.availibleYears.indexOf(e.target.value)>=0
+        ),
+      })
+    }
+  }
+
+
+
   render () {
     return (
       <div className="container">
           <nav><navBar/></nav>
           <div id="content">
-            Countries
-            <Countries countries={this.state.countries}></Countries>
+            <Filter
+              count = {this.state.countries.length}
+              year={this.state.n}
+              sort={this.state.sort}
+              filterCountries={this.filterCountries}
+              sortCountries={this.sortCountries}
+            ></Filter>
+
+            <Countries 
+              countries={this.state.countries}>
+            </Countries>
             </div>
           <div id="sidebar">Cart Items</div>
       </div>
